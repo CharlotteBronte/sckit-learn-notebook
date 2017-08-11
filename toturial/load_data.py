6d0665f462e368b7d701815bdf20de32c0715545
liuluxin <0_0mirror@sina.com>
2017-08-11 09:59:05 +0800

    update

---
+++ toturial/load_data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+"""
+@desc: sckit-learn文件的读写  
+@time: 2017/08/11 05：19
+@author: lucy(0_0mirror@sina.com)
+@param: 
+@output:
+“""
